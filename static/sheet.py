1817a2105abd69d672fce91dd4fae42529cf1b69
Charles Patterson Cook <cook.char.p@gmail.com>
2021-03-20 22:12:48 -0400

    Added hit point controls

--- static/sheet.py
+++ static/sheet.py
@@ -104,6 +104,127 @@ for button in document.select(".abilityButton"):
 	#print(button)
 	button.bind("click", adjustAbilityScore)
 
+def makeDieDict(count : int, die : int) -> dict:
+	return {"count": count, "die": die}
+
+def makeDieString(count : int, die : int) -> str:
+	return str(count) + 'd' + str(die)
+
+def makeDiceString(dieDictionaries : list) -> str:
+	return " + ".join([makeDieString(die["count"], die["die"]) for die in dieDictionaries])
+
+def toggleHitAdjustment(event):
+	for button in document.select(".hitButton"):
+		if event.target.checked:
+			del button.attrs["disabled"]
+		else:
+			button.attrs["disabled"] = ''
+
+document["hitEdit"].bind("change", toggleHitAdjustment)
+
+def syncHitPoints(field, newValue):
+	document[field].value = newValue
+	field = field.split("Hit")[0]
+	data["hit"][field] = newValue
+
+	if field == "max":
+		newCurrent = min(data["hit"]["max"], data["hit"]["current"])
+		document["currentHit"].value = newCurrent
+		data["hit"]["current"] = newCurrent
+
+def adjustHitPoints(event):
+	field = event.target.id.split('`')[0]
+	method = event.target.id.split('`')[1]
+	fieldKeyword = field.split("Hit")[0]
+	oldValue = data["hit"][fieldKeyword]
+
+	if method == "Increment":
+		if field == "maxHit" or field == "currentHit" \
+			and oldValue + 1 <= data["hit"]["max"]:
+			newValue = oldValue + 1
+		else:
+			newValue = oldValue
+	elif method == "Decrement":
+		if field == "maxHit" or field == "currentHit" \
+			and oldValue - 1 >= -data["hit"]["max"]:
+			newValue = oldValue - 1
+		else:
+			newValue = oldValue
+	elif method == "Set":	
+		newValueDialog = dialog.EntryDialog(
+			"Set " + fieldKeyword.capitalize() + " Hit Points",
+			"Enter a new " + fieldKeyword + " hit points value"
+		)
+		def entryHandler(e):
+			newValue = newValueDialog.value
+			newValueDialog.close()
+			try:
+				newValue = int(newValue)
+				if newValue < 1 and field == "maxHit":
+					dialog.InfoDialog(
+						"Hit Points Set Error",
+						"Max Hit Points Must Be Positive & Non-Zero!"
+					)
+				elif newValue < -data["hit"]["max"]:
+					dialog.InfoDialog(
+						"Hit Point Set Error",
+						"Current Hit Points Cannot Fall That Far!"
+					)
+				elif field == "maxHit" or field == "currentHit" \
+					and newValue <= data["hit"]["max"]:
+					syncHitPoints(field, newValue)
+				else:
+					dialog.InfoDialog(
+						"Hit Points Set Error",
+						"Current Hit Points Cannot Exceed Maximum!"
+					)
+			except ValueError:
+				dialog.InfoDialog(
+					"Hit Points Set Error",
+					"Could not parse value as integer!"
+				)
+
+		newValueDialog.bind("entry", entryHandler)
+		return
+	else:
+		newValue = oldValue
+	syncHitPoints(field, newValue)
+
+for button in document.select(".hitButton"):
+	button.bind("click", adjustHitPoints)
+
+def processDeathSaves(event):
+	kind = event.target.id.split('`')[0]
+	count = int(event.target.id.split('`')[1])
+
+	#print(kind + str(count), "disabled" in document[kind + '`' + str(count)].attrs)
+
+	if count < 3:
+		if event.target.checked:
+			try:
+				del document[kind + '`' + str(count + 1)].attrs["disabled"]
+			except KeyError:
+				pass
+
+		else:
+			document[kind + '`' + str(count + 1)].attrs["disabled"] = ''
+			for box in range(count + 1, 4):
+				document[kind + '`' + str(box)].checked = False
+			count -= 1
+	elif not event.target.checked:
+			count -= 1
+	
+	if kind == "success":
+		kind += "es"
+	else:
+		kind += 's'
+
+	data["hit"]["deathSaves"][kind] = count
+	#print(data["hit"]["deathSaves"])
+
+for box in document.select(".deathSaveCheckbox"):
+	box.bind("change", processDeathSaves)
+
 def reloadValues():
 	global data
 	for k in data["biography"].keys():
@@ -119,9 +240,36 @@ def reloadValues():
 				+ ' ' + data["biography"][k]["unit"]
 			#print("(height/weight)")
 
+	#print(data["hit"]["deathSaves"])
+
 	for k in data["abilities"].keys():
 		document[k].value = data["abilities"][k]["score"]
 		document[k + "Bonus"].value = data["abilities"][k]["bonus"]
+
+	document["currentHit"].value = data["hit"]["current"]
+	document["maxHit"].value = data["hit"]["max"]
+
+	try:
+		del document["success`1"].attrs["disabled"]
+	except KeyError:
+		pass
+	for box in range(1, data["hit"]["deathSaves"]["successes"] + 1):
+		if document["success`" + str(box)].checked:
+			break
+		document["success`" + str(box)].checked = True
+		if box < 3:
+			del document["success`" + str(box + 1)].attrs["disabled"]
+
+	try:
+		del document["failure`1"].attrs["disabled"]
+	except KeyError:
+		pass
+	for box in range(1, data["hit"]["deathSaves"]["failures"] + 1):
+		if document["failure`" + str(box)].checked:
+			break
+		document["failure`" + str(box)].checked = True
+		if box < 3:
+			del document["failure`" + str(box + 1)].attrs["disabled"]
 
 def jsonHandler(response):
 	global data
